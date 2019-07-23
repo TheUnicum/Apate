b048b0192032f32f879f70a705a42fcab28a3160
Mattia <tia_ben@tin.it>
2019-07-24 00:32:53 +0200

    Added spdlog and basic logging API/macros.

--- Apate/src/Apate.h
+++ Apate/src/Apate.h
@@ -3,6 +3,7 @@
 // For use by Apate application
 
 #include "Apate/Application.h"
+#include "Apate/Log.h"
 
 // ---Entry Point---------------------
 #include "Apate/EntryPoint.h"
--- Apate/src/Apate/EntryPoint.h
+++ Apate/src/Apate/EntryPoint.h
@@ -6,6 +6,11 @@ extern Apate::Application* Apate::CreateApplication();
 
 int main(int argc, char** arv)
 {
+	Apate::Log::Init();
+	AP_CORE_WARN("Initialize Log!");
+	int a = 5;
+	AP_INFO("Hello! Var={0}", a);
+
 	auto app = Apate::CreateApplication();
 	app->Run();
 	delete app;
