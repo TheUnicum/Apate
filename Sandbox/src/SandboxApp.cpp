9dc89059f5f92e3a3adae04d4538b66b5469f86c
Mattia <tia_ben@tin.it>
2019-08-04 18:38:51 +0200

    [38] Transforms.

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -2,6 +2,8 @@
 
 #include "imgui/imgui.h"
 
+#include <glm/gtc/matrix_transform.hpp>
+
 class ExampleLayer : public Apate::Layer
 {
 public:
@@ -33,10 +35,10 @@ public:
 		m_SquareVA.reset(Apate::VertexArray::Create());
 
 		float squareVertices[3 * 4] = {
-			-0.75f, -0.75f, 0.0f,
-			+0.75f, -0.75f, 0.0f,
-			+0.75f, +0.75f, 0.0f,
-			-0.75f, +0.75f, 0.0f,
+			-0.5f, -0.5f, 0.0f,
+			+0.5f, -0.5f, 0.0f,
+			+0.5f, +0.5f, 0.0f,
+			-0.5f, +0.5f, 0.0f,
 		};
 
 		std::shared_ptr<Apate::VertexBuffer> squareVB;
@@ -58,6 +60,7 @@ public:
 				layout(location = 1) in vec4 a_Color;
 
 				uniform mat4 u_ViewProjection;
+				uniform mat4 u_Transform;
 
 				out vec3 v_Position;
 				out vec4 v_Color;
@@ -66,7 +69,7 @@ public:
 				{
 					v_Position = a_Position;
 					v_Color = a_Color;
-					gl_Position = u_ViewProjection * vec4(a_Position, 1.0f);
+					gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0f);
 				}
 		)";
 
@@ -93,13 +96,14 @@ public:
 				layout(location = 0) in vec3 a_Position;
 
 				uniform mat4 u_ViewProjection;
+				uniform mat4 u_Transform;
 
 				out vec3 v_Position;
 
 				void main()
 				{
 					v_Position = a_Position;
-					gl_Position = u_ViewProjection * vec4(a_Position, 1.0f);
+					gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0f);
 				}
 		)";
 
@@ -121,8 +125,6 @@ public:
 
 	void OnUpdate(Apate::Timestep ts) override
 	{
-		AP_TRACE("Delta time: {0}s ({1}ms)", ts.GetSecond(), ts.GetMillisecond());
-
 		if (Apate::Input::IsKeyPressed(AP_KEY_LEFT))
 			m_CameraPosition.x -= m_CameraMoveSpeed * ts;
 		else if (Apate::Input::IsKeyPressed(AP_KEY_RIGHT))
@@ -146,7 +148,18 @@ public:
 
 		Apate::Renderer::BeginScene(m_Camera);
 
-		Apate::Renderer::Submit(m_BlueShader, m_SquareVA);
+		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+
+		for (int y = 0; y < 20; y++)
+		{
+			for (int i = 0; i < 20; i++)
+			{
+				glm::vec3 pos(i * 0.11, y * 0.11, 0.0f);
+				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+				Apate::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+			}
+		}
+
 		Apate::Renderer::Submit(m_Shader, m_VertexArray);
 
 		Apate::Renderer::EndScene();
